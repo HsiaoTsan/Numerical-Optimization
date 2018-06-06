990c69eead3f1a22ec8f6af8746a02cd81425261
HsiaoTsan <engineerbrucelee@gmail.com>
2018-06-06 23:17:34 +0800

    Create DFP.m

---
+++ BFGS-DFP_Method/DFP.m
@@ -0,0 +1,50 @@
+% DFP
+clc;clear;
+x = [1;1];
+tol = 1e-6;
+H = eye(size(x,1)); % H = inverse Hessian
+I = eye(size(x,1));
+[~,g0,B]=F_Grad_Hessian(x);
+k=0;
+Xrec=[];
+
+while norm(g0)>tol
+k=k+1;
+Xrec=[Xrec, x];
+p = -H*g0; % H = inverse Hessian
+% alpha = LineSearchWolfe(x, p);
+AlphaFun = @(alpha)obj_function(x+alpha*p);
+MinAlpha = fminsearch(AlphaFun,1);
+s = MinAlpha*p;
+[~,g,~]=F_Grad_Hessian(x+s);
+y = g - g0;
+g0 = g;
+rho = 1/(y'*s);
+H = H -H*y*y'*H/(y'*H*y) + s*s'/(y'*s);     
+rho = 1/(y'*s);
+B = (I - rho*s*y')'*B*(I - rho*s*y') + rho*y*y';
+% H = (I - rho*s*y')*B*(I - rho*y'*s) + rho*s*s';
+% B = B - B*s*s'*B/(s'*B*s) + y*y'/(y'*s);
+x = x+s;
+
+end
+
+
+
+
+t1 = linspace(-1.5,1.5);
+t2 = linspace(-1.5,1.5);
+[T1,T2] = meshgrid(t1,t2);
+Z = T1.^2+2*T2.^2;
+
+figure
+contour(T1,T2,Z)
+
+hold on;
+plot(Xrec(1,:),Xrec(2,:) ,'r','linewidth',2);
+plot(Xrec(1,:),Xrec(2,:) ,'*r','markersize',10);
+
+
+title('DFP Method')
+xlabel('t_1')
+ylabel('t_2')
